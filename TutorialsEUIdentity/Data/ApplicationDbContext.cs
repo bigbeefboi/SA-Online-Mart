73511e1a5761a9a4e9f1d1618ba42453eccb0373
Faiz Pathan <139258262+bigbeefboi@users.noreply.github.com>
2024-08-28 20:08:12 +0200

    cartItemModel and updated db contex
    added a model for the shopping cart feature and added the dbset function to the db context and accounted for the foreign key in the cart model

--- TutorialsEUIdentity/Data/ApplicationDbContext.cs
+++ TutorialsEUIdentity/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TutorialsEUIdentity.Models;
 
 namespace TutorialsEUIdentity.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -13,5 +14,17 @@ namespace TutorialsEUIdentity.Data
         }
         //
         public DbSet<ProductModel> Product {  get; set; }
+        public DbSet<CartItemModel> CartItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<CartItemModel>()
+                .HasOne(c => c.Product)
+                .WithMany()
+                .HasForeignKey(c => c.ProductID)
+                .HasPrincipalKey(p => p.Id);
+        }
     }
 }
