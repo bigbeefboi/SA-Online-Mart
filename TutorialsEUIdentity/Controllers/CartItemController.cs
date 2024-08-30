9ecaae07921602b5e28243d3e483034d6f75813f
Faiz Pathan <139258262+bigbeefboi@users.noreply.github.com>
2024-08-30 05:28:15 +0200

    cart completed
    shopping is functional as intended

--- TutorialsEUIdentity/Controllers/CartItemController.cs
+++ TutorialsEUIdentity/Controllers/CartItemController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TutorialsEUIdentity.Data;
 using TutorialsEUIdentity.Models;
+using TutorialsEUIdentity.Services;
+using TutorialsEUIdentity.ViewModels;
 
 namespace TutorialsEUIdentity.Controllers
 {
@@ -14,147 +16,101 @@ namespace TutorialsEUIdentity.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public CartItemController(ApplicationDbContext context)
+        // used for components that are statically rendered on the server
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CartItemController(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         // GET: CartItem
-        public async Task<IActionResult> Index()
+        public ActionResult Index()
         {
-            var applicationDbContext = _context.CartItems.Include(c => c.Product);
-            return View(await applicationDbContext.ToListAsync());
+            var cart = CartItemActions.GetCart(_context, _httpContextAccessor);
+
+            var viewModel = new CartItemViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartItemTotal = cart.GetCartTotal()
+            };
+
+            ViewData["ItemQuantity"] = cart.GetCartItems().Count();
+            return View(viewModel);
+          
         }
 
-        // GET: CartItem/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public ActionResult CartItemSummary()
+        {
+            var cart = CartItemActions.GetCart(_context, _httpContextAccessor);
+            ViewData["ItemQuantity"] =cart.GetItemCount();
+            return PartialView("ItemOverview");
+        }
+
+        public IActionResult AddItemToCart(int pId)
+        {
+           
+            var addedProduct = _context.Product.SingleOrDefault(product => product.Id == pId);
+
+           
+            if (addedProduct == null)
+            {
+              
+                return NotFound(); 
+            }
+
+            var cart = CartItemActions.GetCart(_context, _httpContextAccessor);
+
+            // Add the product to the cart
+            cart.AddToCart(addedProduct);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> RemoveItemFromCart(int id)
+        {
+            var cart = CartItemActions.GetCart(_context, _httpContextAccessor);
+
+            var remainingItems = cart.RemoveItemFromCart(id); 
+
+            if (remainingItems == 0)
+            {
+                
+                return RedirectToAction("Index");
+            }
+
+           
+            return RedirectToAction("Index");
+        }
+
+        //clears cart
+        public IActionResult ClearEntireCart()
+        {
+            var cart = CartItemActions.GetCart(_context, _httpContextAccessor);
+
+            cart.ClearCart();
+
+            return RedirectToAction("Index");
+        }
+
+
+        /*
+        public async Task<IActionResult> RemoveItemFromCart(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
-            var cartItemModel = await _context.CartItems
-                .Include(c => c.Product)
-                .FirstOrDefaultAsync(m => m.CartItemId == id);
-            if (cartItemModel == null)
+            var cartModel =await _context.CartItems.FindAsync(id);
+            if (cartModel != null)
             {
-                return NotFound();
+                _context.CartItems.Remove(cartModel);
             }
-
-            return View(cartItemModel);
-        }
-
-        // GET: CartItem/Create
-        public IActionResult Create()
-        {
-            ViewData["ProductID"] = new SelectList(_context.Product, "Id", "Id");
-            return View();
-        }
-
-        // POST: CartItem/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CartItemId,CartId,Quantity,DateCreated,ProductID")] CartItemModel cartItemModel)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(cartItemModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ProductID"] = new SelectList(_context.Product, "Id", "Id", cartItemModel.ProductID);
-            return View(cartItemModel);
-        }
-
-        // GET: CartItem/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cartItemModel = await _context.CartItems.FindAsync(id);
-            if (cartItemModel == null)
-            {
-                return NotFound();
-            }
-            ViewData["ProductID"] = new SelectList(_context.Product, "Id", "Id", cartItemModel.ProductID);
-            return View(cartItemModel);
-        }
-
-        // POST: CartItem/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CartItemId,CartId,Quantity,DateCreated,ProductID")] CartItemModel cartItemModel)
-        {
-            if (id != cartItemModel.CartItemId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cartItemModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CartItemModelExists(cartItemModel.CartItemId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ProductID"] = new SelectList(_context.Product, "Id", "Id", cartItemModel.ProductID);
-            return View(cartItemModel);
-        }
-
-        // GET: CartItem/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cartItemModel = await _context.CartItems
-                .Include(c => c.Product)
-                .FirstOrDefaultAsync(m => m.CartItemId == id);
-            if (cartItemModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(cartItemModel);
-        }
-
-        // POST: CartItem/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var cartItemModel = await _context.CartItems.FindAsync(id);
-            if (cartItemModel != null)
-            {
-                _context.CartItems.Remove(cartItemModel);
-            }
-
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToActionPermanent(nameof(Index));
         }
+        */
 
         private bool CartItemModelExists(int id)
         {
---
+++ TutorialsEUIdentity/Services/CartItemActions.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorialsEUIdentity.Data;
+using TutorialsEUIdentity.Models;
+
+namespace TutorialsEUIdentity.Services
+{
+    public partial class CartItemActions
+    {
+        // partial class
+        // split the definition of a class into multiple files
+        ApplicationDbContext _context;
+
+        Guid SCartId { get; set; }
+
+        IHttpContextAccessor _httpContextAccessor;
+
+        public const string SessionKey = "CartId";
+
+        public CartItemActions(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public static CartItemActions GetCart(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            var cart = new CartItemActions(context, httpContextAccessor);
+            cart.SCartId = Guid.Parse(cart.GetCartId());
+            return cart;
+        }
+
+        public void AddToCart(ProductModel productModel)
+        {
+            var cartItem = _context.CartItems.SingleOrDefault(
+                c => c.CartId == SCartId
+                && c.ProductID == productModel.Id
+                );
+            if( cartItem == null )
+            {
+                cartItem = new CartItemModel
+                {
+                    ProductID = productModel.Id,
+                    CartId = SCartId,
+                    Quantity = 1,
+                    DateCreated = DateTime.Now,
+                    Product = productModel,
+                };
+                _context.CartItems.Add( cartItem );
+            }
+            else
+            {
+                cartItem.Quantity++;
+            }
+            _context.SaveChanges();
+        }
+
+        public string GetCartId()
+        {
+            var context = _httpContextAccessor.HttpContext;
+            var session = context.Session;
+
+            var cartId = session.GetString(SessionKey);
+            
+            if(string.IsNullOrEmpty(cartId))
+            {
+                cartId = Guid.NewGuid().ToString();
+                session.SetString(SessionKey, cartId);
+            }
+            return context.Session.GetString(SessionKey).ToString();
+        }
+
+        public int GetItemCount()
+        {
+            var count = _context.CartItems
+                .Where(cartItems => cartItems.CartId == SCartId)
+                .Sum(cartItems => (int?)cartItems.Quantity);
+            return count ?? 0;
+        }
+
+        public List<CartItemModel> GetCartItems()
+        {
+            return _context.CartItems
+                .Include(cartItems => cartItems.Product)
+                .Where(cartItems => cartItems.CartId == SCartId)
+                .ToList();
+        }
+
+        public int GetCartTotal()
+        {
+            int? total = (from cartItems in _context.CartItems
+                          where cartItems.CartId == SCartId
+                          select (int?)cartItems.Quantity *
+                          cartItems.Product.Price).Sum();
+            return total ?? int.MaxValue;
+        }
+        public void ClearCart()
+        {
+            var cartItems = _context.CartItems.Where(
+                cart => cart.CartId == SCartId);
+
+            foreach(var cartItem in cartItems)
+            {
+                _context.CartItems.Remove(cartItem);
+            }
+            _context.SaveChanges();
+        }
+
+        public int RemoveItemFromCart(int id)
+        {
+            var cartItem = _context.CartItems.Single(
+                cart => cart.CartId == SCartId
+                && cart.CartItemId == id);
+
+            int count = 0;
+
+            if(cartItem != null)
+            {
+                if(cartItem.Quantity > 1)
+                {
+                    cartItem.Quantity --;
+                    count = cartItem.Quantity;
+                }
+                else
+                {
+                    _context.Remove(cartItem);
+                }
+                _context.SaveChanges();
+            }
+            return count;
+        }
+
+        /*
+         start order
+          */
+        
+    }
+}
